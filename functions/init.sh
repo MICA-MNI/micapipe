6b318be5735b5157fe286dda4e7d183ba2df0dce
rcruces <raulrcruces@gmail.com>
2020-09-11 17:23:19 -0400

    MAYBE I FIXED the PATHs setting on the init file

--- functions/init.sh
+++ functions/init.sh
@@ -7,44 +7,68 @@
 # Save OLD PATH
 export OLD_PATH=$PATH
 
-# Add your bin paths here.
-afni_path=/data/mica1/01_programs/afni-20.2.06/bin
-ants_path=/data/mica1/01_programs/ants-2.3.4/bin
-mrtrix_path=/data/mica1/01_programs/mrtrix3-3.0.1/bin
+#------------------------------------------------------------------------------#
+# SOFTWARE CONFIGURATION for MICAPIPE
+# Afni binaries
+PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*afni*});IFS=':';echo "${p[*]}";unset IFS)
+export AFNIDIR="/data/mica1/01_programs/afni-20.2.06"
+
+# ANTS binaries
+PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*ants*});IFS=':';echo "${p[*]}";unset IFS)
+export ANTSPATH="/data/mica1/01_programs/ants-2.3.4/bin"
+
+# Workbench binaries
+PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*workbench*});IFS=':';echo "${p[*]}";unset IFS)
 workbench_path=/data/mica1/01_programs/workbench/bin_linux64
-freesurfer_path=/data/mica1/01_programs/Freesurfer-6.0/bin
+
+# ICA-FIX
 FIXPATH=/data_/mica1/01_programs/fix
 
 #------------------------------------------------------------------------------#
+# FreeSurfer 6.0 configuration
+export FREESURFER_HOME=/data/mica1/01_programs/Freesurfer-6.0  && source $FREESURFER_HOME/FreeSurferEnv.sh
+
+#------------------------------------------------------------------------------#
+# FSL 6.0 configuration
+export FSLDIR=/data_/mica1/01_programs/fsl_mica
+. /data_/mica1/01_programs/fsl_mica/etc/fslconf/fsl.sh
+export LD_LIBRARY_PATH="${FSLDIR}/lib"
+
+#-----------------------------------------------------------------------------------#
+# MRtrix3 3.0.1
+# revome any other MRtrix3 version from path
+PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*mrtrix*});IFS=':';echo "${p[*]}";unset IFS)
+mrtrixDir=/data_/mica1/01_programs/mrtrix3-3.0.1
+
+#-----------------------------------------------------------------------------------#
+# Export new PATH with al the necessary binaries
+export PATH="${AFNIDIR}:${ANTSPATH}:${workbench_path}:${FIXPATH}:${FREESURFER_HOME}/bin/:${mrtrixDir}/bin:${mrtrixDir}/lib:${PATH}:${PATH}"
+
+#------------------------------------------------------------------------------#
+# Set the libraries paths for mrtrx and fsl
+export LD_LIBRARY_PATH="${FSLDIR}/bin:${mrtrixDir}/lib"
+
+#------------------------------------------------------------------------------#
 # PYTHON 3.7 configuration
-# REMOVES any other python configuration from the PATH the conda from the PATH  and LD_LIBRARY_PATH variable
+# REMOVES any other python configuration from the PATH the conda from the PATH and LD_LIBRARY_PATH variable
 PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*anaconda*});IFS=':';echo "${p[*]}";unset IFS)
 LD_LIBRARY_PATH=$(IFS=':';p=($LD_LIBRARY_PATH);unset IFS;p=(${p[@]%%*anaconda*});IFS=':';echo "${p[*]}";unset IFS)
 unset PYTHONPATH
 unset PYTHONHOME
-
 # Adds the conda3 path to the env PATH variable
 export PATH="/data_/mica1/01_programs/anaconda/anaconda3/bin:${PATH}"
-
-# Creates the necesary env variables to call python3.7
-export PYTHONPATH="/data_/mica1/01_programs/anaconda/anaconda3/envs/mica_py3.7:data_/mica1/01_programs/anaconda/anaconda3/envs/mica_py3.7/bin"
-export PYTHONHOME="/data_/mica1/01_programs/anaconda/anaconda3"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/data_/mica1/01_programs/anaconda/anaconda3/lib"
-
 # Language utilities
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 #------------------------------------------------------------------------------#
-# FSL donfiguration file
-. /etc/fsl/5.0/fsl.sh
-
+#------------------------------------------------------------------------------#
 # Add the number of threads to use here. Note that this is overwritten by
 # $NSLOTS if it exists (i.e. when running on SGE).
 local_threads=10
 
 # Uncomment this and fill in a temporary directory for a custom temporary directory.
-# This takes priority over the default.
+# This takes priority over the default. <<<<<< WHY this and not /tmp???
 global_temp_directory=/data/mica2/temporaryNetworkProcessing/
 
 # Uncomment this and fill in host host/temporary directories for
@@ -64,7 +88,6 @@ rm -f $tmp_file
 
 # Set basic global variables.
 export MICAPIPE="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # Note: As this file is sourced by mica-pipe, this will return the mica-pipe path NOT the path of this script.
-export PATH=${MICAPIPE}:${script_path}:${afni_path}:${ants_path}:${FIXPATH}:${mrtrix_path}:${workbench_path}:${freesurfer_path}:${PATH}
 if [[ ! -z $NSLOTS ]]; then
     export CORES=$NSLOTS
 else
@@ -82,6 +105,7 @@ for idx in $(seq 0 2 ${#host_temp_dirs[@]}); do
         break
     fi
 done
+
 # If that didn't work, try setting from the global/default instead.
 if [[ -z $tmp ]]; then
     if [[ ! -z $global_temp_directory ]]; then
