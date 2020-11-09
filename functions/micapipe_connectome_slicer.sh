49db0bf3bb8f71736e7d8c30b457c661ed9e93b5
rcruces <raulrcruces@gmail.com>
2020-11-09 15:56:23 -0500

    ADDED flag, help and options to the scriot

--- functions/micapipe_connectome_slicer.sh
+++ functions/micapipe_connectome_slicer.sh
@@ -17,36 +17,102 @@
 #
 # ONLY for scripting and debugging
 # TEST=ON
+#---------------- FUNCTION: HELP ----------------#
+help() {
+echo -e "\033[38;5;141m
+Usage:  `basename $0`\033[0m  \033[38;5;197m-sub\033[0m <subject_id> \033[38;5;197m-out\033[0m <outputDirectory> \033[38;5;197m-bids\033[0m <BIDS-directory>\n
+\t\033[38;5;197m-sub\033[0m 	         : Subject identification (no 'sub-')
+\t\033[38;5;197m-out\033[0m 	         : Output directory for the processed files <derivatives>.
+\t\033[38;5;197m-bids\033[0m 	         : Path to BIDS directory
 
-BIDS=$1
-id=$2
-out=$3
-SES=$4
-PROC=$5
-nocleanup=$6
-here=`pwd`
+\t\033[38;5;197m-ses\033[0m 	         : OPTIONAL flag that indicates the session name (DEFAULT is ses-pre)
+
+WARNING: This script is not optimized for SGE, it runs locally only.
+
+McGill University, MNI, MICA-lab, November 2020
+https://github.com/MICA-MNI/micapipe
+http://mica-mni.github.io/
+"
+}
+
+# Check MICAPIPE
+if [ -z "${MICAPIPE}" ]; then
+echo -e "\033[38;5;1m\n---------------------------------------------------------\n
+[ERROR]... MICAPIPE must be define in your enviroment\033[0m
+           TRY: export MICAPIPE=<github_Directory>/micasoft\n
+\033[38;5;1m---------------------------------------------------------\033[0m\n"; exit 1
+fi
+
+if [ ! -f ${MICAPIPE}/functions/utilities.sh ]; then
+echo -e "\033[38;5;1m\n---------------------------------------------------------\n
+[ERROR]... MICAPIPE is defined, but the PATH is wrong,
+           it should match /micasoft directory\033[0m
+           CHECK PATH to MICAPIPE:
+           $MICAPIPE\n
+\033[38;5;1m---------------------------------------------------------\033[0m\n"; exit 1
+fi
+# Source utilities functions from MICAPIPE
+source ${MICAPIPE}/functions/utilities.sh
+# -----------------------------------------------------------------------------------------------#
+#			ARGUMENTS
+# Create VARIABLES
+for arg in "$@"
+do
+  case "$arg" in
+  -h|-help)
+    help
+    exit 1
+  ;;
+  -sub)
+    id=$2
+    shift;shift
+  ;;
+  -out)
+    out=$2
+    shift;shift
+  ;;
+  -bids)
+    BIDS=$2
+    shift;shift
+  ;;
+  -ses)
+    SES=$2
+    shift;shift
+  ;;
+  -nocleanup)
+    nocleanup=TRUE
+    shift
+  ;;
+  -*)
+    Error "Unknown option ${2}"
+    help
+    exit 1
+  ;;
+    esac
+done
+
+# argument check out & WARNINGS
+arg=($id $out $BIDS)
+if [ "${#arg[@]}" -lt 3 ]; then
+Error "One or more mandatory arguments are missing:"
+Note "-sub " $id
+Note "-out " "$out"
+Note "-bids " "$BIDS"
+help; exit 1; fi
 
 # Get the real path of the Inputs
 out=`realpath $out`
 BIDS=`realpath $BIDS`
 id=${id/sub-/}
+here=`pwd`
 
 #------------------------------------------------------------------------------#
-# qsub configuration
-if [ "$PROC" = "qsub-MICA" ] || [ "$PROC" = "qsub-all.q" ];then
-    export MICAPIPE=/data_/mica1/01_programs/micapipe
-    source ${MICAPIPE}/functions/init.sh;
-fi
-
-# source utilities
-source $MICAPIPE/functions/utilities.sh
-
 # Assigns variables names
 bids_variables $BIDS $id $out $SES
 
 # Check inputs: DWI post TRACTOGRAPHY
 lut_sc="${util_lut}/lut_subcortical-cerebellum_mics.csv"
-tracts=10M # <<<<<<<<<<<<<<<<<< Number of streamlines
+tracts=40M # <<<<<<<<<<<<<<<<<< Number of streamlines
 
 # Check inputs
 if [ ! -f $fod ]; then Error "Subject $id doesn't have FOD:\n\t\tRUN -proc_dwi"; exit; fi
