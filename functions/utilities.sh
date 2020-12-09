e7a35611445ea9301e6debd9ff965e400c273ab2
rcruces <raulrcruces@gmail.com>
2020-12-09 17:50:32 -0500

    ADDED export variables and unset variables function

--- functions/utilities.sh
+++ functions/utilities.sh
@@ -16,7 +16,7 @@ bids_variables() {
   SES=$4
 
   #   Define UTILITIES directories
-  scriptDir=${MICAPIPE}/functions
+  export scriptDir=${MICAPIPE}/functions
   # Directory with the templates for the processing
   export util_MNIvolumes=${MICAPIPE}/MNI152Volumes
   # Directory with all the parcellations
@@ -26,65 +26,65 @@ bids_variables() {
   export util_surface=${MICAPIPE}/surfaces # utilities/resample_fsaverage
   export util_mics=${MICAPIPE}/MICs60_T1-atlas
 
-  subject=sub-${id}
-  subject_dir=$out/${subject}/${SES}     # Output directory
-  subject_bids=${BIDS}/${subject}/${SES} # Input BIDS directory
+  export subject=sub-${id}
+  export subject_dir=$out/${subject}/${SES}     # Output directory
+  export subject_bids=${BIDS}/${subject}/${SES} # Input BIDS directory
 
   # Structural directories derivatives/
-  proc_struct=$subject_dir/proc_struct # structural processing directory
-  	 dir_first=$proc_struct/first      # FSL first
-  	 dir_volum=$proc_struct/volumetric # Cortical segmentations
-  	 dir_surf=$proc_struct/surfaces    # surfaces
-  			     dir_freesurfer=${dir_surf}/${id}  # freesurfer dir
-  			     dir_conte69=${dir_surf}/conte69   # conte69
-  proc_dwi=$subject_dir/proc_dwi               # DWI processing directory
-    dwi_cnntm=$proc_dwi/connectomes
-  proc_rsfmri=$subject_dir/proc_rsfmri
-    rsfmri_ICA=$proc_rsfmri/ICA_MELODIC
-    rsfmri_volum=$proc_rsfmri/volumetric
-    rsfmri_surf=$proc_rsfmri/surfaces
-  dir_warp=$subject_dir/xfms              # Transformation matrices
-  dir_logs=$subject_dir/logs              # directory with log files
-  dir_QC=$subject_dir/QC                  # directory with QC files
-  dir_QC_png=$subject_dir/QC/png                  # directory with QC files
+  export proc_struct=$subject_dir/proc_struct # structural processing directory
+  	 export dir_first=$proc_struct/first      # FSL first
+  	 export dir_volum=$proc_struct/volumetric # Cortical segmentations
+  	 export dir_surf=$proc_struct/surfaces    # surfaces
+  			     export dir_freesurfer=${dir_surf}/${id}  # freesurfer dir
+  			     export dir_conte69=${dir_surf}/conte69   # conte69
+  export proc_dwi=$subject_dir/proc_dwi               # DWI processing directory
+    export dwi_cnntm=$proc_dwi/connectomes
+  export proc_rsfmri=$subject_dir/proc_rsfmri
+    export rsfmri_ICA=$proc_rsfmri/ICA_MELODIC
+    export rsfmri_volum=$proc_rsfmri/volumetric
+    export rsfmri_surf=$proc_rsfmri/surfaces
+  export dir_warp=$subject_dir/xfms              # Transformation matrices
+  export dir_logs=$subject_dir/logs              # directory with log files
+  export dir_QC=$subject_dir/QC                  # directory with QC files
+  export dir_QC_png=$subject_dir/QC/png                  # directory with QC files
 
   # post structural Files (the resolution might vary depending on the dataset)
   if [ -f ${proc_struct}/${id}_t1w_*mm_nativepro.nii.gz ]; then
-    T1nativepro=${proc_struct}/${id}_t1w_*mm_nativepro.nii.gz
-    T1nativepro_brain=${proc_struct}/${id}_t1w_*mm_nativepro_brain.nii.gz
-    T1freesurfr=${dir_freesurfer}/mri/T1.mgz
-    T15ttgen=${proc_struct}/${id}_t1w_*mm_nativepro_5TT.nii.gz
-    T1fast_seg=$proc_struct/first/${id}_t1w_*mm_nativepro_all_fast_firstseg.nii.gz
-    res=`mrinfo ${T1nativepro} -spacing | awk '{printf "%.1f\n", $2}'`
+    export T1nativepro=${proc_struct}/${id}_t1w_*mm_nativepro.nii.gz
+    export T1nativepro_brain=${proc_struct}/${id}_t1w_*mm_nativepro_brain.nii.gz
+    export T1freesurfr=${dir_freesurfer}/mri/T1.mgz
+    export T15ttgen=${proc_struct}/${id}_t1w_*mm_nativepro_5TT.nii.gz
+    export T1fast_seg=$proc_struct/first/${id}_t1w_*mm_nativepro_all_fast_firstseg.nii.gz
+    export res=`mrinfo ${T1nativepro} -spacing | awk '{printf "%.1f\n", $2}'`
   fi
 
   # Native midsurface in gifti format
-  lh_midsurf=${dir_freesurfer}/surf/lh.midthickness.surf.gii
-  rh_midsurf=${dir_freesurfer}/surf/rh.midthickness.surf.gii
+  export lh_midsurf=${dir_freesurfer}/surf/lh.midthickness.surf.gii
+  export rh_midsurf=${dir_freesurfer}/surf/rh.midthickness.surf.gii
 
   # Registration from MNI152 to Native pro
-  T1str_nat=${id}_t1w_${res}mm_nativepro
-  mat_MNI152_SyN=${dir_warp}/${T1str_nat}_brain_to_0.8mm_MNI152_SyN_brain_    # transformation strings nativepro to MNI152_0.8mm
-  T1_MNI152_InvWarp=${mat_MNI152_SyN}1InverseWarp.nii.gz                      # Inversewarp - nativepro to MNI152_0.8mm
-  T1_MNI152_affine=${mat_MNI152_SyN}0GenericAffine.mat
-  MNI152_mask=${util_MNIvolumes}/MNI152_T1_0.8mm_brain_mask.nii.gz
+  export T1str_nat=${id}_t1w_${res}mm_nativepro
+  export mat_MNI152_SyN=${dir_warp}/${T1str_nat}_brain_to_0.8mm_MNI152_SyN_brain_    # transformation strings nativepro to MNI152_0.8mm
+  export T1_MNI152_InvWarp=${mat_MNI152_SyN}1InverseWarp.nii.gz                      # Inversewarp - nativepro to MNI152_0.8mm
+  export T1_MNI152_affine=${mat_MNI152_SyN}0GenericAffine.mat
+  export MNI152_mask=${util_MNIvolumes}/MNI152_T1_0.8mm_brain_mask.nii.gz
 
   # BIDS Files: resting state
-  bids_mainScan=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-AP_*.nii*`)       # main rsfMRI scan
-  bids_mainScanJson=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-AP_*.json`)   # main rsfMRI scan json
-  bids_mainPhase=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-APse*.nii*`)     # main phase scan
-  bids_reversePhase=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-PAse*.nii*`)  # reverse phase scan
+  export bids_mainScan=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-AP_*.nii*`)       # main rsfMRI scan
+  export bids_mainScanJson=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-AP_*.json`)   # main rsfMRI scan json
+  export bids_mainPhase=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-APse*.nii*`)     # main phase scan
+  export bids_reversePhase=(`ls ${subject_bids}/func/${subject}_${SES}_task-rest_acq-PAse*.nii*`)  # reverse phase scan
 
   # Resting state proc files
-  topupConfigFile=${FSLDIR}/etc/flirtsch/b02b0_1.cnf                                    # TOPUP config file default
-  icafixTraining=${MICAPIPE}/functions/MICAMTL_training_15HC_15PX.RData                 # ICA-FIX training file default
+  export topupConfigFile=${FSLDIR}/etc/flirtsch/b02b0_1.cnf                                    # TOPUP config file default
+  export icafixTraining=${MICAPIPE}/functions/MICAMTL_training_15HC_15PX.RData                 # ICA-FIX training file default
 
   # BIDS Files
-  bids_T1ws=(`ls ${subject_bids}/anat/*T1w.nii*`)
-  bids_dwis=(`ls ${subject_bids}/dwi/*acq-b*_dir-*_dwi.nii*`)
-  bids_T1map=${subject_bids}/anat/*mp2rage*.nii*
-  bids_inv1=${subject_bids}/anat/*inv1*.nii*
-  dwi_reverse=${subject_bids}/dwi/*_${SES}_acq-PA_dir-*_dwi.nii*
+  export bids_T1ws=(`ls ${subject_bids}/anat/*T1w.nii*`)
+  export bids_dwis=(`ls ${subject_bids}/dwi/*acq-b*_dir-*_dwi.nii*`)
+  export bids_T1map=${subject_bids}/anat/*mp2rage*.nii*
+  export bids_inv1=${subject_bids}/anat/*inv1*.nii*
+  export dwi_reverse=${subject_bids}/dwi/*_${SES}_acq-PA_dir-*_dwi.nii*
 
 }
 
@@ -163,6 +163,60 @@ bids_print.variables-rsfmri() {
   Note "Main rsfMRI json   =" `find $mainScanJson`
   Note "TOPUP config file  =" `find $topupConfigFile`
   Note "ICA-FIX training   =" `find $icafixTraining`
+}
+
+bids_variables_unset() {
+  # This function unsets all the enviromentalk variables defined by
+  # bids_variables
+  unset $scriptDir
+  unset $util_MNIvolumes
+  unset $util_parcelations
+  unset $util_lut
+  unset $util_surface
+  unset $util_mics
+  unset $subject
+  unset $subject_dir
+  unset $subject_bids
+  unset $proc_struct
+  unset $dir_first
+  unset $dir_volum
+  unset $dir_surf
+  unset $dir_freesurfer
+  unset $dir_conte69
+  unset $proc_dwi
+  unset $dwi_cnntm
+  unset $proc_rsfmri
+  unset $rsfmri_ICA
+  unset $rsfmri_volum
+  unset $rsfmri_surf
+  unset $dir_warp
+  unset $dir_logs
+  unset $dir_QC
+  unset $dir_QC_png
+  unset $T1nativepro
+  unset $T1nativepro_brain
+  unset $T1freesurfr
+  unset $T15ttgen
+  unset $T1fast_seg
+  unset $res
+  unset $lh_midsurf
+  unset $rh_midsurf
+  unset $T1str_nat
+  unset $mat_MNI152_SyN
+  unset $T1_MNI152_InvWarp
+  unset $T1_MNI152_affine
+  unset $MNI152_mask
+  unset $bids_mainScan
+  unset $bids_mainScanJson
+  unset $bids_mainPhase
+  unset $bids_reversePhase
+  unset $topupConfigFile
+  unset $icafixTraining
+  unset $bids_T1ws
+  unset $bids_dwis
+  unset $bids_T1map
+  unset $bids_inv1
+  unset $dwi_reverse
 }
 
 t1w_str() {
@@ -282,5 +336,6 @@ function cleanup() {
   export PATH=$OLD_PATH
   unset OLD_PATH
   rm -rf $tmp
+  bids_variables_unset
   cd $here
 }
