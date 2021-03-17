909bbf8f652adf3e6352b064f2fb1496f9287e64
rcruces <raulrcruces@gmail.com>
2021-03-16 23:19:52 -0400

    bash syntax

--- functions/01_proc-struc_freesurfer.sh
+++ functions/01_proc-struc_freesurfer.sh
@@ -24,7 +24,7 @@ id=$2
 out=$3
 SES=$4
 nocleanup=$5
-threads=$6
+export threads=$6
 tmpDir=$7
 FSdir=$8
 PROC=$9
@@ -34,17 +34,17 @@ here=$(pwd)
 # qsub configuration
 if [ "$PROC" = "qsub-MICA" ] || [ "$PROC" = "qsub-all.q" ];then
     export MICAPIPE=/data_/mica1/01_programs/micapipe
-    source ${MICAPIPE}/functions/init.sh;
+    source "${MICAPIPE}"/functions/init.sh;
 fi
 
 # source utilities
-source $MICAPIPE/functions/utilities.sh
+source "$MICAPIPE/functions/utilities.sh"
 
 # Assigns variables names
 bids_variables "$BIDS" "$id" "$out" "$SES"
 
 # Stop if freesurfer has finished without errors
-if grep -q "finished without error" ${dir_freesurfer}/scripts/recon-all.log; then
+if grep -q "finished without error" "${dir_freesurfer}/scripts/recon-all.log"; then
 Warning "Subject ${id} has Freesurfer
                     > If you want to re-run for QC purposes try it manually
                     > If you want to run again this step first erase all the outputs with:
@@ -59,17 +59,20 @@ micapipe_software
 bids_print.variables
 Info "Saving temporal dir: $nocleanup\n\t\t\t\t\t${tmpDir}"
 
+#	Timer
+aloita=$(date +%s)
+
 # # Create script specific temp directory
-tmp=${tmpDir}/${RANDOM}_micapipe_proc-freesurfer_${id}
-Do_cmd mkdir -p $tmp
+tmp="${tmpDir}/${RANDOM}_micapipe_proc-freesurfer_${id}"
+Do_cmd mkdir -p "$tmp"
 
 # TRAP in case the script fails
 trap 'cleanup $tmp $nocleanup $here' SIGINT SIGTERM
 
-if [ -d $FSdir ]; then
+if [ -d "$FSdir" ]; then
     Info "Copying from freesurfer_dir"
-    Do_cmd mkdir $dir_freesurfer
-    Do_cmd cp -Rf $FSdir/* $dir_freesurfer
+    Do_cmd mkdir "$dir_freesurfer"
+    Do_cmd cp -Rf "$FSdir"/* "$dir_freesurfer"
 
 elif [[ "$FSdir" = "FALSE" ]]; then
     Info "Running Freesurfer"
@@ -82,39 +85,44 @@ elif [[ "$FSdir" = "FALSE" ]]; then
     # Define SUBJECTS_DIR for freesurfer processing as a global variable
     # Will work on a temporal directory
     export SUBJECTS_DIR=${tmp}
-    if [ ! -d ${tmp}/nii ]; then Do_cmd mkdir ${tmp}/nii; fi
+    if [ ! -d "$tmp/nii" ]; then Do_cmd mkdir "$tmp/nii"; fi
 
     # Copy all the T1 from the BIDS directory to the TMP
     # transform to NIFTI (if == NIFTI_GZ)
     for t1 in ${bids_T1ws[@]}; do
-        t1_name=`echo $t1 | awk -F 'anat/' '{print $2}'`
-        if [[ $t1 == *'.gz'* ]]; then
-              Do_cmd fslchfiletype NIFTI $t1 ${tmp}/nii/${t1_name/.gz/}
-        else  Do_cmd cp $t1 ${tmp}/nii/
+        t1_name=$(echo "$t1" | awk -F 'anat/' '{print $2}')
+        if [[ "$t1" == *'.gz'* ]]; then
+              Do_cmd fslchfiletype NIFTI "$t1" "$tmp/nii/${t1_name/.gz/}"
+        else  Do_cmd cp "$t1" "$tmp/nii/"
         fi
     done
 
     # List of Files for processing
-    fs_cmd=$(echo "-i $(echo ${tmp}/nii/*nii | sed 's: : -i :g')")
+    fs_cmd=$(echo "-i $(echo "$tmp"/nii/*nii | sed 's: : -i :g')")
 
     # Perform recon-all surface registration
     Do_cmd recon-all -cm -all "$fs_cmd" -s "$id"
 
     # Copy the recon-all log to our MICA-log Directory
-    Do_cmd cp -v ${tmp}/${id}/scripts/recon-all.log ${dir_logs}/recon-all.log
+    Do_cmd cp -v "${tmp}/${id}/scripts/recon-all.log" "${dir_logs}/recon-all.log"
 
     # Copy results to  freesurfer's SUBJECTS_DIR directory
-    Do_cmd cp -rv ${tmp}/${id} $dir_surf
+    Do_cmd cp -rv "${tmp}/${id}" "$dir_surf"
 
     Info "Check log file:\n\t\t\t ${dir_logs}/recon-all.log"
 fi
 
-
+# -----------------------------------------------------------------------------------------------
 # Notification of completition
-if [ -f ${dir_freesurfer}/mri/T1.mgz ]; then status="COMPLETED"; else status="ERROR"; fi
+if [ -f "${dir_freesurfer}/mri/T1.mgz" ]; then status="COMPLETED"; else status="ERROR"; fi
+
+# QC notification of completition
+lopuu=$(date +%s)
+eri=$(echo "$lopuu - $aloita" | bc)
+eri=$(echo print "$eri"/60 | perl)
 
 Title "Freesurfer recon-all processing ended: ${status}\n\tlogs:
-`ls ${dir_logs}/proc-freesurfer*.txt`"
+$(ls "$dir_logs"/proc_freesurfer*.txt)"
 
-echo "${id}, FREESURFER, ${status}, $(whoami), $(uname -n), $(date), $(printf "%0.3f\n" ${eri}), $PROC" >> ${out}/brain-proc.csv
-cleanup $tmp $nocleanup $here
+echo "${id}, ${SES/ses-/}, proc_freesurfer, ${status}, $(whoami), $(uname -n), $(date), $(printf "%0.3f\n" "$eri"), $PROC" >> "${out}/brain-proc.csv"
+cleanup "$tmp" "$nocleanup" "$here"
