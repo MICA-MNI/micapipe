067f88c13d8dc0441e5f972bbd19a614c5457717
rcruces <raulrcruces@gmail.com>
2021-06-08 21:04:04 -0400

    [ENH] update print Info

--- functions/micapipe_qc.R
+++ functions/micapipe_qc.R
@@ -259,50 +259,50 @@ rgloptions=list("windowRect"=c(80,80,800,800))
 for (annot in c('aparc', 'aparc.a2009s')) {
   nom <- paste0(BIDSid,"_space-fsnative_",annot,"_desc-qc.png"); nom.png <- paste0(dir_QC_png,"/",nom)
   if (!file.exists(nom.png)) {
-    print(paste0("INFO.... Creating PNG of ", annot, " on native surface"))
+    print(paste0("[INFO].... Creating PNG of ", annot, " on native surface"))
     cm <- vis.subject.annot(dir_fs, BIDSid, annot, 'both', surface="pial", rglactions= list('no_vis'=T))
     img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, img_only = TRUE, grid_like = FALSE, view_angles = view_angles);
     while (rgl.cur() > 0) { rgl.close() }
   } else {
-    print(paste0("INFO.... File exists: ", nom))
+    print(paste0("[INFO].... File exists: ", nom))
   }
 }
 
 ####  fs - Sulcal depth ####
 nom <-  paste0(BIDSid,"_space-fsnative_desc-qc_sulc.png"); nom.png <- paste0(dir_QC_png,"/",nom)
 if (!file.exists(nom.png)) {
-  print(paste0("INFO.... Creating PNG of the SULCAL DEPTH on native surface"))
+  print(paste0("[INFO].... Creating PNG of the SULCAL DEPTH on native surface"))
   rgla <- list('trans_fun'=limit_fun(-5, 5), 'no_vis'=T);
   cm <- vis.subject.morph.native(dir_fs, BIDSid, 'sulc', cortex_only = FALSE,
                                  draw_colorbar = TRUE, surface="white", rglactions = rgla, makecmap_options = list('colFn'=cividis))
   img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Sulcal depth [mm]', grid_like = FALSE, view_angles = view_angles);
   while (rgl.cur() > 0) { rgl.close() }
-} else { print(paste0("INFO.... File exists: ", nom)) }
+} else { print(paste0("[INFO].... File exists: ", nom)) }
 
 
 ####  fs - Cortical thickness ####
 nom <-  paste0(BIDSid,"_space-fsnative_desc-qc_thickness.png"); nom.png <- paste0(dir_QC_png,"/",nom)
 if (!file.exists(nom.png)) {
-  print(paste0("INFO.... Creating PNG of the THICKNESS on native surface"))
+  print(paste0("[INFO].... Creating PNG of the THICKNESS on native surface"))
   rgla <- list('trans_fun'=limit_fun(1.5, 4), 'no_vis'=T);
   cm <- vis.subject.morph.native(dir_fs, BIDSid, 'thickness', cortex_only = FALSE,
                                  draw_colorbar = TRUE, surface="pial", rglactions = rgla, makecmap_options = list('colFn'=inferno))
   img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Thickness [mm]', grid_like = FALSE, view_angles = view_angles);
   while (rgl.cur() > 0) { rgl.close() }
-} else { print(paste0("INFO.... File exists: ", nom)) }
+} else { print(paste0("[INFO].... File exists: ", nom)) }
 
 
 #### fs - Curvature ####
 col.curv <- colorRampPalette(c('darkolivegreen3', 'gray50', 'firebrick3'));
 nom <-  paste0(BIDSid,"_space-fsnative_desc-qc_curv.png"); nom.png <- paste0(dir_QC_png,"/",nom)
 if (!file.exists(nom.png)) {
-  print(paste0("INFO.... Creating PNG of the CURVATURE on native surface"))
+  print(paste0("[INFO].... Creating PNG of the CURVATURE on native surface"))
   rgla <- list('trans_fun'=limit_fun(-0.2, 0.2), 'no_vis'=T);
   cm <- vis.subject.morph.native(dir_fs, BIDSid, 'curv', cortex_only = FALSE,
                                  draw_colorbar = TRUE, surface="white", rglactions = rgla, makecmap_options = list('colFn'=col.curv))
   img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Curvature', grid_like = FALSE, view_angles = view_angles);
   while (rgl.cur() > 0) { rgl.close() }
-} else { print(paste0("INFO.... File exists: ", nom)) }
+} else { print(paste0("[INFO].... File exists: ", nom)) }
 
 
 # -----------------------------------------------------
@@ -313,13 +313,13 @@ th.lh <- paste0(dir_morpho,"/",BIDSid,"_space-fsnative_desc-lh_thickness.mgh")
 th.rh <- paste0(dir_morpho,"/",BIDSid,"_space-fsnative_desc-rh_thickness.mgh")
 if (file.exists(th.lh) && file.exists(th.rh) ) {
   if (!file.exists(nom.png)) {
-    print(paste0("INFO.... Creating ",nom, " of the THICKNESS on native surface"))
+    print(paste0("[INFO].... Creating ",nom, " of the THICKNESS on native surface"))
     rgla <- list('trans_fun'=limit_fun(1.5, 4), 'no_vis'=T);
     cm <- vis.data.on.subject(dir_fs, BIDSid, morph_data_lh=th.lh, morph_data_rh=th.rh,
                               draw_colorbar = TRUE, surface="pial", rglactions = rgla,  makecmap_options = list('colFn'=inferno))
     img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Thickness [mm]', grid_like = FALSE, view_angles = view_angles);
     while (rgl.cur() > 0) { rgl.close() }
-  } else { print(paste0("INFO.... File exists: ", nom)) }
+  } else { print(paste0("[INFO].... File exists: ", nom)) }
 }
 
 # fsaverage5 thickness
@@ -328,13 +328,13 @@ th.lh.fs5 <- paste0(dir_morpho,"/",BIDSid,"_space-fsaverage5_desc-lh_thickness.m
 th.rh.fs5 <- paste0(dir_morpho,"/",BIDSid,"_space-fsaverage5_desc-rh_thickness.mgh")
 if (file.exists(th.lh.fs5) && file.exists(th.rh.fs5)) {
   if (!file.exists(nom.png)) {
-    print(paste0("INFO.... Creating ",nom, " of the THICKNESS on fsaverage5"))
+    print(paste0("[INFO].... Creating ",nom, " of the THICKNESS on fsaverage5"))
     rgla <- list('trans_fun'=limit_fun(1.5, 4), 'no_vis'=T);
     cm <- vis.data.on.subject(dir_fs, 'fsaverage5', morph_data_lh=th.lh.fs5, morph_data_rh=th.rh.fs5,
                               draw_colorbar = TRUE, surface="pial", rglactions = rgla,  makecmap_options = list('colFn'=inferno))
     img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Thickness [mm]', grid_like = FALSE, view_angles = view_angles);
     while (rgl.cur() > 0) { rgl.close() }
-  } else { print(paste0("INFO.... File exists: ", nom)) }
+  } else { print(paste0("[INFO].... File exists: ", nom)) }
 }
 
 # -----------------------------------------------------
@@ -344,13 +344,13 @@ cv.lh <- paste0(dir_morpho,"/",BIDSid,"_space-fsnative_desc-lh_curvature.mgh")
 cv.rh <- paste0(dir_morpho,"/",BIDSid,"_space-fsnative_desc-rh_curvature.mgh")
 if (file.exists(cv.lh) && file.exists(cv.rh)) {
   if (!file.exists(nom.png)) {
-    print(paste0("INFO.... Creating PNG of the CURVATURE on native surface"))
+    print(paste0("[INFO].... Creating PNG of the CURVATURE on native surface"))
     rgla <- list('trans_fun'=limit_fun(-0.3, 0.3), 'no_vis'=T);
     cm <- vis.data.on.subject(dir_fs, BIDSid, morph_data_lh=cv.lh, morph_data_rh=cv.rh,
                               draw_colorbar = 'horizontal', surface="inflated",  rglactions=rgla, makecmap_options = list('colFn'=col.curv))
     img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Curvature', grid_like = FALSE, view_angles = view_angles);
     while (rgl.cur() > 0) { rgl.close() }
-  } else { print(paste0("INFO.... File exists: ", nom)) }
+  } else { print(paste0("[INFO].... File exists: ", nom)) }
 }
 
 # fsaverage5 curvature
@@ -359,13 +359,13 @@ cv.lh.fs5 <- paste0(dir_morpho,"/",BIDSid,"_space-fsaverage5_desc-lh_curvature.m
 cv.rh.fs5 <- paste0(dir_morpho,"/",BIDSid,"_space-fsaverage5_desc-rh_curvature.mgh")
 if (file.exists(cv.lh.fs5) && file.exists(cv.rh.fs5)) {
   if (!file.exists(nom.png)) {
-    print(paste0("INFO.... Creating Creating PNG of the CURVATURE on fsaverage5"))
+    print(paste0("[INFO].... Creating Creating PNG of the CURVATURE on fsaverage5"))
     rgla <- list('trans_fun'=limit_fun(-0.3, 0.3), 'no_vis'=T);
     cm <- vis.data.on.subject(dir_fs, 'fsaverage5', morph_data_lh=cv.lh.fs5, morph_data_rh=cv.rh.fs5,
                               draw_colorbar = TRUE, surface="inflated", rglactions = rgla,  makecmap_options = list('colFn'=col.curv))
     img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = 'Curvature', grid_like = FALSE, view_angles = view_angles);
     while (rgl.cur() > 0) { rgl.close() }
-  } else { print(paste0("INFO.... File exists: ", nom)) }
+  } else { print(paste0("[INFO].... File exists: ", nom)) }
 }
 
 
@@ -385,13 +385,13 @@ for (i in 1:14) {
                     rh=freesurferformats::read.fs.morph(mpc.rh)*mask.rh )
       # Calculate color range (quatile 20% to 95%) >>>> rgla <- list('trans_fun'=limit_fun(1200,2000), 'no_vis'=T);
       Qt <- round(quantile(c(morph$lh[morph$lh!=0], morph$rh[morph$rh!=0]), probs = c(0.05,0.95)),1)
-      print(paste0("INFO.... Creating PNG of MPC-", i0))
+      print(paste0("[INFO].... Creating PNG of MPC-", i0))
       rgla <- list('trans_fun'=limit_fun(Qt[1],Qt[2]), 'no_vis'=T);
       cm <- vis.data.on.subject(dir_fs, BIDSid, morph_data_lh=morph$lh, morph_data_rh=morph$rh,
                                 surface = "pial", draw_colorbar = 'horizontal', rglactions = rgla)
       img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, colorbar_legend = paste0('MPC-',i0), grid_like = FALSE, view_angles = view_angles);
       while (rgl.cur() > 0) { rgl.close() }
-    } else { print(paste0("INFO.... File exists: ", nom)) }
+    } else { print(paste0("[INFO].... File exists: ", nom)) }
   }
 }
 
@@ -402,12 +402,12 @@ atlas <- gsub(".annot", "", atlas)
 for (annot in atlas) {
   nom <- paste0(BIDSid,"_atlas-",annot,"_desc-qc.png"); nom.png <- paste0(dir_QC_png,"/",nom)
   if (!file.exists(nom.png)) {
-    print(paste0("INFO.... Creating PNG of ", annot, " on native surface"))
+    print(paste0("[INFO].... Creating PNG of ", annot, " on native surface"))
     cm <- vis.subject.annot(dir_fs, BIDSid, annot, 'both', surface="pial", rglactions= list('no_vis'=T))
     img <- vis.export.from.coloredmeshes(cm, output_img = nom.png, img_only = TRUE, grid_like = FALSE, view_angles = view_angles, colorbar_legend = annot);
     while (rgl.cur() > 0) { rgl.close() }
   } else {
-    print(paste0("INFO.... File exists: ", nom))
+    print(paste0("[INFO].... File exists: ", nom))
   }
 }
 if (file.exists("fsbrain_cbar.png")==TRUE) {file.remove("fsbrain_cbar.png")}
