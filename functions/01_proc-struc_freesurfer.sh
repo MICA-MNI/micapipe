c0c3bcbb1eb605a05c7b280dc221bb76e3216e1f
Raúl RC <rcruces@users.noreply.github.com>
2022-05-16 14:37:06 -0400

    [ENH] add N4 weighted with white matter mask to the hires input of freesurfer

--- functions/01_proc-struc_freesurfer.sh
+++ functions/01_proc-struc_freesurfer.sh
@@ -113,7 +113,12 @@ elif [[ "$FSdir" == "FALSE" ]]; then
         Info "Running recon with native submillimeter resolution"
         export EXPERT_FILE=${tmp}/expert.opts
         echo "mris_inflate -n 100" > "$EXPERT_FILE"
-        Do_cmd recon-all -all -s "$idBIDS" -hires -i "$T1nativepro" -expert "$EXPERT_FILE"
+        # Optimize the contrast of the T1nativepro
+        pve2=T1nativepro=${proc_struct}/${idBIDS}_space-nativepro_t1w_brain_pve_2.nii.gz
+        T1_n4="${tmp}/${idBIDS}_space-nativepro_t1w_N4w.nii.gz"
+        Do_cmd N4BiasFieldCorrection -r -d 3 -w ${pve2} -i "$T1nativepro" -o "$T1n4"
+        # Apply non-local means denoising
+        Do_cmd recon-all -all -s "$idBIDS" -hires -i "$T1_n4" -expert "$EXPERT_FILE"
         # Fix the inflation
         Do_cmd mris_inflate -n 15 "${tmp}/${idBIDS}"/surf/?h.smoothwm "${tmp}/${idBIDS}"/surf/?h.inflated
     else
