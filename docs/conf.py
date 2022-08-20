dd05e68d558589760049ddf5206830db268f3c34
Raúl RC <rcruces@users.noreply.github.com>
2022-08-20 01:06:29 -0400

    again

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Raul-Rodriguez-Cruces, Jessica Royer, Janie Degré-Pelletier, Sara Lar
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx-tabs.tabs',
+extensions = ['sphinx_tabs.tabs',
               'sphinx.ext.autodoc',
               'sphinx.ext.autosectionlabel',
               'sphinx.ext.autosummary',
