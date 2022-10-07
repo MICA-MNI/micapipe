91f2c15b8cb54b050a17ec91a12ba1d5758f5cdc
Raúl RC <rcruces@users.noreply.github.com>
2022-10-07 19:25:07 -0400

    UPDATED version

--- functions/utilities.sh
+++ functions/utilities.sh
@@ -3,7 +3,7 @@
 # MICA BIDS structural processing
 #
 # Utilities
-export Version="v0.1.2 'Roadrunner'"
+export Version="v0.2.0 'Roadrunner'"
 
 bids_variables() {
   # This functions assignes variables names acording to:
