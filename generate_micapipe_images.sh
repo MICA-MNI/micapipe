58afed18050cc9ef047d68d6ef6030f23fae64a8
rcruces <raulrcruces@gmail.com>
2022-11-15 18:34:06 -0500

    CHANGE entry point to micapipe, update brainspace version

--- generate_micapipe_images.sh
+++ generate_micapipe_images.sh
@@ -26,7 +26,7 @@ generate() {
                      pillow==7.2.0 pyparsing==2.4.7 python-dateutil==2.8.1
                      pytz==2020.1 scikit-learn==0.23.2 scipy==1.5.2
                      six==1.15.0 threadpoolctl==2.1.0 vtk==9.0.1"\
-      pip_install='brainspace==0.1.2 argparse==1.1' \
+      pip_install='brainspace==0.1.4 argparse==1.1' \
       create_env="micapipe" \
       activate=true \
     --run-bash 'source activate micapipe && conda install -c mrtrix3 mrtrix3==3.0.1 && pip install git+https://github.com/MICA-MNI/ENIGMA.git' \
@@ -48,7 +48,7 @@ generate() {
     --env MICAPIPE='/opt/micapipe'\
     --add-to-entrypoint "source /opt/freesurfer-7.3.2/SetUpFreeSurfer.sh" \
     --add-to-entrypoint "export FIXPATH=/opt/fix && export PATH="${FIXPATH}:${PATH}"" \
-    --entrypoint "/neurodocker/startup.sh /opt/micapipe/mica-pipe"
+    --entrypoint "/neurodocker/startup.sh /opt/micapipe/micapipe"
   }
 
 
