351cf0de44c0684aae70dd193920e60b211246ee
rcruces <raulrcruces@gmail.com>
2023-02-15 09:30:39 -0500

    create script to rename multi acquisitions MPC and func

---
+++ functions/rename_log.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# MICAPIPE  function that renames multiple acquisitions log files
+
+log_file_str="$1"
+labelStr="$2"
+randStr="$3"
+if [ -f "${log_file_str}".txt ]; then
+    new_log_str="${log_file_str/-${randStr}/}"
+    tagMRI=$(grep "${labelStr}" "${log_file_str}".txt | awk -F ':' 'NR==1 {print $2}'| tr -d '[:space:]' | sed -r "s/\x1B\[(([0-9]+)(;[0-9]+)*)?[m,K,H,f,J]//g")
+    mv -v "${log_file_str}".txt "${new_log_str}_${tagMRI}".txt && if [[ -f ${log_file_str}*_.txt ]]; then rm ${log_file_str}*_.txt; fi
+    if [ -f "${log_file_str}".e ]; then mv "${log_file_str}".e "${new_log_str}-${tagMRI}".e; fi
+    if [[ -f ${log_file_str}*_.e ]]; then rm ${log_file_str}*_.e; fi
+fi
