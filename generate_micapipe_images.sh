a0991f912a2aa36e4da1f683e1e9daaf27f9be7f
rcruces <raulrcruces@gmail.com>
2023-05-19 23:29:44 -0400

    succesful built

--- generate_micapipe_images.sh
+++ generate_micapipe_images.sh
@@ -26,15 +26,31 @@ generate() {
     --run-bash "cd /opt/ && wget http://www.fmrib.ox.ac.uk/~steve/ftp/fix1.068.tar.gz && tar xvfz fix1.068.tar.gz && rm fix1.068.tar.gz" \
     --user=mica \
     --miniconda \
-      conda_install="python=3.9.13 certifi==2020.6.20
-                     cycler==0.10.0 joblib==0.16.0
-                     kiwisolver==1.2.0 matplotlib==3.4.3 nibabel==4.0.2
-                     numpy==1.21.5 packaging==20.4 pandas==1.4.4
-                     pillow==7.2.0 pyparsing==2.4.7 python-dateutil==2.8.1
-                     pytz==2020.1 scikit-learn==1.0.2 scipy==1.9.1
-                     six==1.15.0 threadpoolctl==2.1.0 vtk==9.2.2"\
-      pip_install='brainspace==0.1.4 seaborn==0.11.2 tedana==0.0.12 pygeodesic==0.1.8 xhtml2pdf==0.2.9' \
-      pip_install='sys os glob argparse==1.1 copy time re warnings subprocess' \
+      version=22.11.1 \
+      conda_install="python=3.9.13 aiohttp==3.8.4 aiosignal==1.3.1
+                    asn1crypto==1.5.1 async-timeout==4.0.2 attrs==22.2.0
+                    bokeh==2.2.3  cffi==1.15.1 charset-normalizer==3.1.0 click==8.1.3 contourpy==1.0.7
+                    cryptography==39.0.2 cycler==0.11.0
+                    fonttools==4.39.2 frozenlist==1.3.3 html5lib==1.1
+                    idna==3.4 importlib-resources==5.12.0 jinja2==3.0.1
+                    joblib==1.2.0 kiwisolver==1.4.4 lxml==4.9.2
+                    markupsafe==2.1.2 matplotlib==3.4.3
+                    multidict==6.0.4 nibabel==4.0.2 nilearn==0.10.0
+                    numpy==1.21.5 packaging==23.0
+                    pandas==1.4.4 pillow==9.4.0 pycparser==2.21
+                    pyhanko-certvalidator==0.20.1
+                    pyparsing==3.0.9 pypdf==3.6.0 pypng==0.20220715.0
+                    python-bidi==0.4.2 python-dateutil==2.8.2 pytz==2022.7.1
+                    pytz-deprecation-shim==0.1.0.post0 pyyaml==6.0 qrcode==7.4.2
+                    reportlab==3.6.12 requests==2.28.2 scikit-learn==1.0.2
+                    scipy==1.9.1 seaborn==0.11.2 six==1.16.0
+                    svglib==1.5.1 threadpoolctl==3.1.0
+                    tinycss2==1.2.1 tornado==6.2 typing-extensions==4.5.0
+                    tzlocal==4.3 uritools==4.0.1 urllib3==1.26.15
+                    vtk==9.2.2 webencodings==0.5.1 wslink==1.10.1 yarl==1.8.2 zipp==3.15.0 " \
+      pip_install='argparse==1.1 brainspace==0.1.4 tedana==0.0.12 pyhanko==0.17.2 mapca==0.0.3
+                    xhtml2pdf==0.2.9 oscrypto==1.3.0 tzdata==2022.7 arabic-reshaper==3.0.0
+                    cssselect2==0.7.0 pygeodesic==0.1.8' \
       create_env="micapipe" \
       activate=true \
     --run-bash 'source activate micapipe && conda install -c mrtrix3 mrtrix3==3.0.1 && pip install git+https://github.com/MICA-MNI/ENIGMA.git' \
@@ -60,7 +76,6 @@ generate() {
     --workdir='/home/mica' \
     --env MICAPIPE='/opt/micapipe'\
     --env PROC='container-micapipe v0.2.0' \
-    --add-to-entrypoint "source /opt/freesurfer-7.4.0/SetUpFreeSurfer.sh" \
     --add-to-entrypoint "export FIXPATH=/opt/fix && export PATH="${FIXPATH}:${PATH}"" \
     --entrypoint "/neurodocker/startup.sh /opt/micapipe/micapipe"
   }
@@ -83,8 +98,11 @@ NOTES:
 -  RUN bash -c 'bash /opt/fsl-6.0.3/etc/fslconf/fslpython_install.sh -f /opt/fsl-6.0.3'
 
 > libxp6 issue
-&& curl -sSL --retry 5 -o /tmp/libxp6_1.0.2-2_amd64.deb http://launchpadlibrarian.net/160108232/libxp6_1.0.2-1ubuntu1_amd64.deb \
-&& dpkg -i /tmp/libxp6_1.0.2-2_amd64.deb \
-&& rm /tmp/libxp6_1.0.2-2_amd64.deb \
+-  replaCE : http://mirrors.kernel.org/debian/pool/main/libx/libxp/libxp6_1.0.2-2_amd64.deb
+-      WITH: http://launchpadlibrarian.net/160108232/libxp6_1.0.2-1ubuntu1_amd64.deb
+
+> Miniconda
+- https://repo.continuum.io/miniconda/Miniconda3-22.11.1-Linux-x86_64.sh
+- https://repo.anaconda.com/miniconda/Miniconda3-py39_22.11.1-1-Linux-x86_64.sh
 
 ###########################################################################################\n"
