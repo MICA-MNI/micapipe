55fddd4cd3945a3360ab965e887b40f95174d315
rcruces <raulrcruces@gmail.com>
2023-12-20 02:39:47 -0500

    Fixed registrations, ERASED comments, TRIED to make surface_generator.py work, but it still does not generate 15 surfaces

--- functions/03_MPC-SWM.sh
+++ functions/03_MPC-SWM.sh
@@ -106,32 +106,36 @@ export SUBJECTS_DIR="$dir_surf"
 outDir="${subject_dir}/mpc-swm/${mpc_p}"
 Note "acqMRI:" "${mpc_str}"
 
+# Check if the directory exists and change the permissions
+[[ ! -d "$outDir" ]] && mkdir -p "$outDir" && chmod -R 770 "$outDir"
+# Create the MPC-SWM module json file
+json_mpc "$microImage" "${outDir}/${idBIDS}_MPC-SWM_${mpc_str}.json"
+
 # json file
 #qt1_json="${dir_maps}/${idBIDS}_space-nativepro_map-${mpc_str}.json"
 
 #------------------------------------------------------------------------------#
 # Registration between both images
-T1_in_fs=${tmp}/orig.nii.gz
-qT1_fsnative=${proc_struct}/${idBIDS}_space-fsnative_${mpc_str}.nii.gz
+qT1_nativepro=${proc_struct}/${idBIDS}_space-nativepro_${mpc_str}.nii.gz
 
 # Affine transformations
-str_qMRI2fs_xfm="${dir_warp}/${idBIDS}_from-${mpc_str}_to-fsnative"
-mat_qMRI2fs_xfm="${str_qMRI2fs_xfm}0GenericAffine.mat"
+str_qMRI2np_xfm="${dir_warp}/${idBIDS}_from-${mpc_str}_to-nativepro_"
+mat_qMRI2np_xfm="${str_qMRI2np_xfm}0GenericAffine.mat"
 
 # SyN_transformations
-SyN_qMRI2fs_warp="${str_qMRI2fs_xfm}1Warp.nii.gz"
-SyN_qMRI2fs_Invwarp="${str_qMRI2fs_xfm}1InverseWarp.nii.gz"
+SyN_qMRI2np_warp="${str_qMRI2np_xfm}1Warp.nii.gz"
+SyN_qMRI2np_Invwarp="${str_qMRI2np_xfm}1InverseWarp.nii.gz"
 
 # Apply transformations
 if [[ ${reg_nonlinear}  == "TRUE" ]]; then
     # SyN from T1_nativepro to t1-nativepro
     export reg="s"
-    transformsInv="-t [${mat_qMRI2fs_xfm},1] -t ${SyN_qMRI2fs_Invwarp}" # T1_fsnative to qMRI
-    transforms="-t ${SyN_qMRI2fs_warp} -t ${mat_qMRI2fs_xfm}"  # qMRI to T1_fsnative T1_fsnative to qMRI
+    transformsInv="-t [${mat_qMRI2np_xfm},1] -t ${SyN_qMRI2np_Invwarp}" # T1nativepro to qMRI
+    transforms="-t ${SyN_qMRI2np_warp} -t ${mat_qMRI2np_xfm}"  # qMRI to T1nativepro T1nativepro to qMRI
 else
     export reg="a"
-    transformsInv="-t [${mat_qMRI2fs_xfm},1]"  # T1_fsnative to qMRI
-    transforms="-t ${mat_qMRI2fs_xfm}"   # qMRI to T1_fsnative
+    transformsInv="-t [${mat_qMRI2np_xfm},1]"  # T1nativepro to qMRI
+    transforms="-t ${mat_qMRI2np_xfm}"   # qMRI to T1nativepro
 fi
 
 synthseg_native() {
@@ -142,53 +146,53 @@ synthseg_native() {
   Do_cmd fslmaths "${tmp}/${mri_str}_synthseg.nii.gz" -uthr 42 -thr 42 -bin -mul -39 -add "${tmp}/${mri_str}_synthseg.nii.gz" "${mri_synth}"
 }
 
-# Calculate the restristations
-if [[ ! -f "$qT1_fsnative" ]] || [[ ! -f "$mat_qMRI2fs_xfm" ]]; then ((N++))
-    img_fixed="${T1_in_fs}"
+# Calculate the registrations
+if [[ ! -f "$qT1_nativepro" ]] || [[ ! -f "$mat_qMRI2np_xfm" ]]; then ((N++))
+    img_fixed="${T1nativepro}"
     img_moving="${regImage}"
 
-    # copy orig.nii.gz from fsurfer to tmp
-    Do_cmd mrconvert "$T1surf" "$T1_in_fs"
     # Registration with synthseg
     if [[ "${synth_reg}" == "TRUE" ]]; then
       Info "Running label based affine registrations"
-      synthseg_native "${T1_in_fs}" "T1w"
+      synthseg_native "${T1nativepro}" "T1w"
       synthseg_native "${regImage}" "qT1"
       img_fixed="${tmp}/T1w_synthsegGM.nii.gz"
       img_moving="${tmp}/qT1_synthsegGM.nii.gz"
     fi
 
     # Registrations from t1-fsnative to qMRI
-    Do_cmd antsRegistrationSyN.sh -d 3 -f "$img_fixed" -m "$img_moving" -o "$str_qMRI2fs_xfm" -t "${reg}" -n "$threads" -p d -i ["${img_fixed}","${img_moving}",0]
+    Do_cmd antsRegistrationSyN.sh -d 3 -f "$img_fixed" -m "$img_moving" -o "$str_qMRI2np_xfm" -t "${reg}" -n "$threads" -p d -i ["${img_fixed}","${img_moving}",0]
+    rm "${dir_warp}/${idBIDS}"*_Warped.nii.gz
 
-    # Check if transfo ations file exist
-    if [ ! -f "${mat_qMRI2fs_xfm}" ]; then Error "Registration between ${mpc_str} and T1nativepro FAILED. Check you inputs!"; cleanup "$tmp" "$nocleanup" "$here"; exit; fi
+    # Check if transformation file exist
+    if [ ! -f "${mat_qMRI2np_xfm}" ]; then Error "Registration between ${mpc_str} and T1nativepro FAILED. Check you inputs!"; cleanup "$tmp" "$nocleanup" "$here"; exit; fi
 
     # Apply transformations: from qMRI to T1-fsnative
-    Do_cmd antsApplyTransforms -d 3 -i "$microImage" -r "$T1_in_fs" "${transforms}" -o "$qT1_fsnative" -v -u int
-    if [[ -f ${qT1_fsnative} ]]; then ((Nsteps++)); fi
+    Do_cmd antsApplyTransforms -d 3 -i "$microImage" -r "$T1nativepro" "${transforms}" -o "$qT1_nativepro" -v -u int
+    if [[ -f ${qT1_nativepro} ]]; then ((Nsteps++)); fi
 else
     Info "Subject ${id} has a ${mpc_str} on Surface space"; ((Nsteps++)); ((N++))
 fi
 
 # Convert the ANTs transformation file for wb_command
 wb_affine="${tmp}/${idBIDS}_from-fsnative_to_qMRI_wb.mat"
-Do_cmd c3d_affine_tool -itk "${mat_qMRI2fs_xfm}" -o "${wb_affine} -inv"
+Do_cmd c3d_affine_tool -itk "${mat_qMRI2np_xfm}" -o "${wb_affine} -inv"
 
 #------------------------------------------------------------------------------#
-# Check if the directory exists and change the permissions
-[[ ! -d "$outDir" ]] && mkdir -p "$outDir" && chmod -R 770 "$outDir"
-# Create the MPC-SWM module json file
-json_mpc "$microImage" "${outDir}/${idBIDS}_MPC-SWM_${mpc_str}.json"
-
 # Laplacian surface generation
-num_surfs=15
-thickness=0.2
+num_surfs=15 #15, 10
+thickness=0.2 #0.2, 0.3
+
 # Define 15 surfaces in 3mm separated by 0.2mm step (3 mm=0.2mm step * 15 surfaces)
 deepths=($(seq "${thickness}" "${thickness}" 3))
+
 # Replace the blanck space with comma
-printf -v deepths_comma '%s,' "${deepths[@]}"
-deepths_comma=$(echo "${deepths_comma%,}")
+for ((i = 0; i <= 15; i += 2)); do sliced1+=("${deepths[i]}"); done
+for ((i = 1; i <= 14; i += 2)); do sliced2+=("${deepths[i]}"); done
+printf -v deepths1_comma '%s,' "${sliced1[@]}"
+printf -v deepths2_comma '%s,' "${sliced2[@]}"
+deepths1_comma=$(echo "${deepths1_comma%,}")
+deepths2_comma=$(echo "${deepths2_comma%,}")
 
 # Run the SWM generation ig the fsLR-5k MPC does not exist
 MPC_fsLR5k="${outDir}/${idBIDS}_surf-fsLR-5k_desc-MPC.shape.gii"
@@ -198,6 +202,7 @@ if [[ ! -f "${MPC_fsLR5k}" ]]; then ((N++))
     Do_cmd mri_convert "${dir_subjsurf}/mri/aparc+aseg.mgz" "${T1fs_seg}"
 
     # Move the segmentation to T1_nativepro space
+    # tranformations where calculated on -post_structural
     mat_fsnative_affine="${dir_warp}/${idBIDS}_from-fsnative_to_nativepro_T1w_"
     T1_fsnative_affine="${mat_fsnative_affine}0GenericAffine.mat"
     T1nativepro_seg="${tmp}/aparc+aseg_space-nativepro.nii.gz"
@@ -214,7 +219,8 @@ if [[ ! -f "${MPC_fsLR5k}" ]]; then ((N++))
       # Prepare the white matter surface
       Do_cmd cp "${dir_conte69}/${idBIDS}_hemi-${HEMI}_space-nativepro_surf-fsnative_label-white.surf.gii ${tmp}/${HEMI}_wm.surf.gii"
       # Run SWM
-      Do_cmd python "${MICAPIPE}"/functions/surface_generator.py "${tmp}/${HEMI}_wm.surf.gii" "${WM_laplace}" "${tmp}/${idBIDS}_hemi-${HEMI}_space-nativepro_surf-fsnative_label-swm" "${deepths_comma}"
+      Do_cmd python "${MICAPIPE}"/functions/surface_generator.py "${tmp}/${HEMI}_wm.surf.gii" "${WM_laplace}" "${tmp}/${idBIDS}_hemi-${HEMI}_space-nativepro_surf-fsnative_label-swm" "${deepths1_comma}"
+      Do_cmd python "${MICAPIPE}"/functions/surface_generator.py "${tmp}/${HEMI}_wm.surf.gii" "${WM_laplace}" "${tmp}/${idBIDS}_hemi-${HEMI}_space-nativepro_surf-fsnative_label-swm" "${deepths2_comma}"
 
       # find all laplacian surfaces and list by creation time
       for i in ${!deepths[@]} ; do
@@ -231,69 +237,16 @@ if [[ ! -f "${MPC_fsLR5k}" ]]; then ((N++))
           # Apply transformation to register surface to nativepro
           wb_command -surface-apply-affine "${surf_swm}" "${wb_affine}" "${out_surf}"
           # Apply Non-linear Warpfield to register surface to nativepro
-          if [[ ${reg_nonlinear}  == "TRUE" ]]; then Do_cmd wb_command -surface-apply-warpfield "${out_surf}" "${SyN_qMRI2fs_Invwarp}" "${out_surf}"; fi
+          if [[ ${reg_nonlinear}  == "TRUE" ]]; then Do_cmd wb_command -surface-apply-warpfield "${out_surf}" "${SyN_qMRI2np_Invwarp}" "${out_surf}"; fi
           # Sample intensity and resample to other surfaces fsaverage5, fsLR-32k and fsLR-5k
           map_to-surfaces "${microImage}" "${out_surf}" "${out_feat}" "${HEMI}" "${label}" "${outDir}"
        done
     done
     Nwm=$(ls "${tmp}/${idBIDS}_hemi-"*_space-qMRI_surf-fsnative_label-swm*.surf.gii 2>/dev/null | wc -l)
-    if [[ "$Nwm" -ge 30 ]]; then ((Nsteps++)); fi
+    if [[ "$Nwm" -ge $((num_surfs*2)) ]]; then ((Nsteps++)); fi
 else
     Info "Subject ${idBIDS} has SWM surfaces"; ((Nsteps++)); ((N++))
 fi
-
-#------------------------------------------------------------------------------#
-### qT1 registration to nativepro ###
-# Register nativepro and qt1
-# T1_fsnative_affine="${dir_warp}/${idBIDS}_from-fsnative_to_nativepro_T1w_0GenericAffine.mat"
-#
-# qmriNP="${dir_maps}/${idBIDS}_space-nativepro_map-${mpc_str}.nii.gz"
-# if [[ ! -f "$qmriNP" ]]; then
-#   Info "${mpc_str} registration to nativepro"
-#     Do_cmd antsApplyTransforms -d 3 -i "$microImage" -r "$T1nativepro_brain" -t "${T1_fsnative_affine}" "${transforms}" -o "$qmriNP" -v -u float
-#     ((Nsteps++)); ((N++))
-# else
-#     Info "Subject ${id} ${mpc_str} is registered to nativepro"; ((Nsteps++)); ((N++))
-# fi
-
-# Write json file
-#json_nativepro_qt1 "$qmriNP" \
-#    "antsApplyTransforms -d 3 -i ${microImage} -r ${T1nativepro_brain} -t ${T1_fsnative_affine} ${transforms} -o ${qmriNP} -v -u float" \
-#    "$qt1_json"
-
-#------------------------------------------------------------------------------#
-# # Map to surface: midthickness, white
-# Nmorph=$(ls "${dir_maps}/"*"${mpc_str}"*gii 2>/dev/null | wc -l)
-# if [[ "$Nmorph" -lt 16 ]]; then ((N++))
-#     Info "Mapping ${mpc_str} to fsLR-32k, fsLR-5k and fsaverage5"
-#     for HEMI in L R; do
-#         for label in midthickness white; do
-#             surf_fsnative="${dir_conte69}/${idBIDS}_hemi-${HEMI}_space-nativepro_surf-fsnative_label-${label}.surf.gii"
-#             # MAPPING metric to surfaces
-#             map_to-surfaces "${qmriNP}" "${surf_fsnative}" "${dir_maps}/${idBIDS}_hemi-${HEMI}_surf-fsnative_label-${label}_${mpc_str}.func.gii" "${HEMI}" "${label}_${mpc_str}" "${dir_maps}"
-#         done
-#     done
-#     Nmorph=$(ls "${dir_maps}/"*${mpc_str}*gii 2>/dev/null | wc -l)
-#     if [[ "$Nmorph" -eq 16 ]]; then ((Nsteps++)); fi
-# else
-#     Info "Subject ${idBIDS} has ${mpc_str} mapped to surfaces"; ((Nsteps++)); ((N++))
-# fi
-
-# # Map to surface: swm depths
-# maps=(${dir_maps}/*nii*)
-# for map in ${maps[*]}; do
-#   map_id=$(echo ${map/.nii.gz/} | awk -F 'map-' '{print $2}')
-#   # Map to surface: swm
-#       for HEMI in L R; do
-#           for i in $(ls "${dir_conte69}/${idBIDS}_hemi-L"_surf-fsnative_label-swm*mm.surf.gii); do
-#               label=$(echo ${i/.surf.gii/} | awk -F 'label-' '{print $2}')
-#               Info "Mapping ${map_id} SWM-${label} to fsLR-32k, fsLR-5k and fsaverage5"
-#               surf_fsnative="${dir_conte69}/${idBIDS}_hemi-${HEMI}_surf-fsnative_label-${label}.surf.gii"
-#               # MAPPING metric to surfaces
-#               map_to-surfaces "${map}" "${surf_fsnative}" "${dir_maps}/${idBIDS}_hemi-${HEMI}_surf-fsnative_label-${label}_${map_id}.func.gii" "${HEMI}" "${label}_${map_id}" "${dir_maps}"
-#           done
-#       done
-# done
 
 #------------------------------------------------------------------------------#
 # Create MPC connectomes and Intensity profiles per parcellations
@@ -316,7 +269,6 @@ if [[ ! -f "${MPC_fsLR5k}" ]]; then ((N++))
   Do_cmd python "$MICAPIPE"/functions/build_mpc-vertex.py "$out" "$id" "$SES" "${mpc_p}" "mpc-swm" "$num_surfs"
   if [[ -f "${MPC_fsLR5k}" ]]; then ((Nsteps++)); fi
 else Info "Subject ${id} has MPC vertex-wise on fsLR-5k"; ((Nsteps++)); ((N++)); fi
-rm "${dir_warp}/${idBIDS}"*_Warped.nii.gz
 
 #------------------------------------------------------------------------------#
 # QC notification of completition
