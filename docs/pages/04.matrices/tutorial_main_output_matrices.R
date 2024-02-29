6649c23ae51a65807c4f8d70b4eaf80b73ae751d
cerysojj <cerysjenkins2@gmail.com>
2024-02-29 15:00:38 -0500

    Changed the working directory to the subject's directory

--- docs/pages/04.matrices/tutorial_main_output_matrices.R
+++ docs/pages/04.matrices/tutorial_main_output_matrices.R
@@ -11,13 +11,11 @@ require('RColorBrewer')      # version 1.1-2
 require('viridis')           # version 0.5.1
 require('gifti')             # version 0.8.0
 
-# Set the working directory to out directory
+# Set the working directory to your subject's directory
+setwd("out/micapipe_v0.2.0/sub-HC001/ses-01")
 
 # This variable will be different for each subject
-sub <- 'HC001'
-ses <- '01'
-subjectID <- paste0('sub-',sub,'_ses-',ses) # <<<<<<<<<<<< CHANGE THIS SUBJECT's ID
-subjectDir <- paste0('micapipe_v0.2.0/sub-',sub,'/ses-',ses) # <<<<<<<<<<<< CHANGE THIS SUBJECT's DIRECTORY
+subjectID <- 'sub-HC001_ses-01' # <<<<<<<<<<<< CHANGE THIS SUBJECT's ID
 
 # Here we define the atlas
 atlas <- 'schaefer-400' # <<<<<<<<<<<< CHANGE THIS ATLAS
@@ -28,7 +26,7 @@ atlas <- 'schaefer-400' # <<<<<<<<<<<< CHANGE THIS ATLAS
 
 ### Full structural connectome
 # Set the path to the the structural cortical connectome
-cnt_sc_cor <- paste0(subjectDir, '/dwi/connectomes/', subjectID, '_space-dwi_atlas-', atlas, '_desc-iFOD2-40M-SIFT2_full-connectome.shape.gii')
+cnt_sc_cor <- paste0('dwi/connectomes/', subjectID, '_space-dwi_atlas-', atlas, '_desc-iFOD2-40M-SIFT2_full-connectome.shape.gii')
 
 # Load the cortical connectome
 mtx_sc <- readgii(cnt_sc_cor)$data$shape
@@ -42,7 +40,7 @@ image(log(mtx_sc), axes=FALSE, main=paste0("SC ", atlas), col=brewer.pal(9, "Pur
 
 ###  Full structural connectome edge lengths
 # Set the path to the the structural cortical connectome
-cnt_sc_EL <- paste0(subjectDir, '/dwi/connectomes/', subjectID, '_space-dwi_atlas-', atlas, '_desc-iFOD2-40M-SIFT2_full-edgeLengths.txt')
+cnt_sc_EL <- paste0('dwi/connectomes/', subjectID, '_space-dwi_atlas-', atlas, '_desc-iFOD2-40M-SIFT2_full-edgeLengths.txt')
 
 # Load the cortical connectome
 mtx_scEL <- as.matrix(read.csv(cnt_sc_EL, sep=" ", header=FALSE,))
@@ -59,7 +57,7 @@ image(log(mtx_scEL), axes=FALSE, main=paste0("SC ", atlas), col=brewer.pal(9, "P
 
 # Set the path to the the functional cortical connectome
 acq_func <- 'desc-se_task-rest_acq-AP_bold'
-cnt_fs <- paste0(subjectDir, '/func/', acq_func, '/surf/', 'sub-',sub,'_ses-',ses, '_surf-fsLR-32k_atlas-', atlas, '_desc-FC.shape.gii')
+cnt_fs <- paste0('func/', acq_func, '/surf/', subjectID, '_surf-fsLR-32k_atlas-', atlas, '_desc-FC.shape.gii')
 
 # Load the cortical connectome
 mtx_fs <- readgii(cnt_fs)$data$shape
@@ -72,7 +70,7 @@ image(mtx_fs, axes=FALSE, main=paste0("FC ", atlas), col=brewer.pal(9, "Reds"))
 
 ### Time series
 # Set the path to the the time series file
-cnt_time <- paste0(subjectDir, '/func/', acq_func, '/surf/', 'sub-',sub,'_ses-',ses, '_surf-fsLR-32k_desc-timeseries_clean.shape.gii')
+cnt_time <- paste0('func/', acq_func, '/surf/', subjectID, '_surf-fsLR-32k_desc-timeseries_clean.shape.gii')
 
 # Load the time series
 mtx_time <- readgii(cnt_time)$data$shape
@@ -84,7 +82,7 @@ image(mtx_time, axes=FALSE, main=paste0("Time series ", atlas), col=plasma(64))
 #### MPC connectomes ####
 
 # Set the path to the the MPC cortical connectome
-cnt_mpc <- paste0(subjectDir, '/mpc/acq-T1map/', subjectID,'_atlas-', atlas, '_desc-MPC.shape.gii')
+cnt_mpc <- paste0('mpc/acq-T1map/', subjectID,'_atlas-', atlas, '_desc-MPC.shape.gii')
 
 # Load the cortical connectome
 mtx_mpc <- readgii(cnt_mpc)$data$shape
@@ -97,7 +95,7 @@ image(mtx_mpc, axes=FALSE, main=paste0("MPC ", atlas), col=brewer.pal(9, "Greens
 
 ### Intensity profiles (Profile x ROI)
 # Set the path to the the time series file
-cnt_int <- paste0(subjectDir, '/mpc/acq-T1map/', subjectID,'_atlas-', atlas, '_desc-intensity_profiles.shape.gii')
+cnt_int <- paste0('mpc/acq-T1map/', subjectID,'_atlas-', atlas, '_desc-intensity_profiles.shape.gii')
 
 # Load the time series
 mtx_int <- readgii(cnt_int)$data$shape
@@ -109,7 +107,7 @@ image(t(mtx_int), axes=FALSE, main=paste0("Intensity profiles ", atlas), col=bre
 #### Geodesic distance connectomes ####
 
 # Set the path to the the geodesic distance connectome
-cnt_gd <- paste0(subjectDir, '/dist/', subjectID, '_atlas-', atlas, '_GD.shape.gii')
+cnt_gd <- paste0('dist/', subjectID, '_atlas-', atlas, '_GD.shape.gii')
 
 # Load the cortical connectome
 mtx_gd <- readgii(cnt_gd)$data$shape
