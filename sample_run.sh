39c8cc162bb0d6909f17cb280e48d6c139e567c7
zihuaihuai <enning2000@gmail.com>
2024-12-05 14:17:32 -0500

    linux sample run

---
+++ sample_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the variables
+SUBJECT_ID="sub-mri3T"
+OUTPUT_DIR="/home/bic/eyang/processed_data"
+BIDS_DIR="/data_/mica3/BIDS_CI/rawdata"
+FS_LIC="$FREESURFER_HOME/license.txt"
+T1W_STR="run-2_T1w"  # Example: processing only 'run-02_T1w'
+MULTI_FACTOR=45  # Example factor for 3T MP2RAGE, adjust if different
+
+# Run micapipe
+micapipe -sub $SUBJECT_ID -out $OUTPUT_DIR -bids $BIDS_DIR \
+         -proc_structural -T1wStr $T1W_STR -mf $MULTI_FACTOR \
+	 -fs_licence $FS_LIC
+
