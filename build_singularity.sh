6465033cfb8aebc526705705a731d07c331b8d94
Enning Yang <enning2000@gmail.com>
2025-10-08 17:06:31 -0400

    fix: Change singularity default output to server build location
    Changed default output directory from:
      /data_/mica1/01_programs/singularity (production location)
    To:
      /host/cassio/export03/data/enning/singularity (build/test location)
    
    This keeps the SIF file in the same location as Docker builds.
    Directory will be created automatically if it doesn't exist.
    
    Users can override by setting SINGULARITY_DIR environment variable:
      SINGULARITY_DIR=/data_/mica1/01_programs/singularity ./build_singularity.sh
    
    After testing, the SIF can be copied to production:
      cp /host/cassio/export03/data/enning/singularity/micapipe_v1_beta.sif \
         /data_/mica1/01_programs/singularity/

--- build_singularity.sh
+++ build_singularity.sh
@@ -19,8 +19,12 @@ set -e  # Exit on error
 # Configuration
 # ============================================================================
 DOCKER_IMAGE="ghcr.io/mica-mni/micapipe"
-SINGULARITY_DIR="/data_/mica1/01_programs/singularity"
+SINGULARITY_DIR="/host/cassio/export03/data/enning/singularity"
 OUTPUT_NAME="micapipe_v1_beta.sif"
+
+# Alternate production location: /data_/mica1/01_programs/singularity
+# You can override SINGULARITY_DIR by setting it before running:
+#   SINGULARITY_DIR=/data_/mica1/01_programs/singularity ./build_singularity.sh
 
 # Get Docker tag from argument or use 'latest'
 DOCKER_TAG="${1:-latest}"
