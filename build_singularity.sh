de54fc50eaa8d3d79ac7ec138b07caead4ea866f
Enning Yang <enning2000@gmail.com>
2025-12-03 13:23:20 -0500

    Change Singularity output filename to v1_beta

--- build_singularity.sh
+++ build_singularity.sh
@@ -12,7 +12,7 @@ FULL_DOCKER_IMAGE="${DOCKER_IMAGE}:${DOCKER_TAG}"
 
 BASE_DIR="/export03/data/enning"
 OUTPUT_DIR="${BASE_DIR}/singularity"
-OUTPUT_PATH="${OUTPUT_DIR}/micapipe_v0.2.3.sif"
+OUTPUT_PATH="${OUTPUT_DIR}/micapipe_v1_beta.sif"
 
 # Performance settings for your 128GB server
 export SINGULARITY_CACHEDIR="${BASE_DIR}/.singularity_cache"
